48cf890d047777a91fa32e815832a2eb6d1723ed
Dory <zoudaoli@163.com>
2016-12-16 17:13:58 +0800

    add convert view to image method

--- Quartz2D/Quartz2D/DoryUIView.m
+++ Quartz2D/Quartz2D/DoryUIView.m
@@ -171,7 +171,16 @@
 //    CGPathRelease(path);
     
     
-    
+    //把画出来的图形保存成图片格式
+//    -(UIImage*)convertViewToImage:(UIView*)v{
+//        CGSize s = v.bounds.size;
+//        // 下面方法，第一个参数表示区域大小。第二个参数表示是否是非透明的。如果需要显示半透明效果，需要传NO，否则传YES。第三个参数就是屏幕密度了
+//        UIGraphicsBeginImageContextWithOptions(s, YES, [UIScreen mainScreen].scale);
+//        [v.layer renderInContext:UIGraphicsGetCurrentContext()];
+//        UIImage*image = UIGraphicsGetImageFromCurrentImageContext();
+//        UIGraphicsEndImageContext();
+//        return image;
+//    }
     
     
 }
